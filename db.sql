59a1da1b53e7f6cf504a6693895f80e2fbcf608e
rehjmov <nijatrahimov9@gmail.com>
2023-08-29 00:18:48 +0400

    first

---
+++ db.sql
@@ -0,0 +1,12 @@
+INSERT INTO question (question_title, first_option, second_option, third_option, fourth_option, right_answer, difficulty_level)
+VALUES
+    ('What is the capital of France?', 'Rome', 'Madrid', 'Paris', 'Berlin', 'Paris', 1),
+    ('Which planet is known as the Red Planet?', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Mars', 2),
+    ('What is the largest mammal on Earth?', 'Elephant', 'Giraffe', 'Blue Whale', 'Polar Bear', 'Blue Whale', 1),
+    ('Who painted the Mona Lisa?', 'Vincent van Gogh', 'Pablo Picasso', 'Leonardo da Vinci', 'Michelangelo', 'Leonardo da Vinci', 2),
+    ('What is the chemical symbol for gold?', 'Go', 'Gl', 'Au', 'Ag', 'Au', 1),
+    ('Which gas do plants use for photosynthesis?', 'Oxygen', 'Nitrogen', 'Carbon Dioxide', 'Hydrogen', 'Carbon Dioxide', 2),
+    ('Which famous scientist developed the theory of general relativity?', 'Isaac Newton', 'Albert Einstein', 'Stephen Hawking', 'Galileo Galilei', 'Albert Einstein', 3),
+    ('What is the largest organ in the human body?', 'Liver', 'Heart', 'Skin', 'Brain', 'Skin', 1),
+    ('Which literary work begins with the line "It was the best of times, it was the worst of times"?', 'Moby-Dick', 'Pride and Prejudice', 'War and Peace', 'A Tale of Two Cities', 'A Tale of Two Cities', 2),
+    ('What is the process of converting water into vapor called?', 'Melting', 'Evaporation', 'Condensation', 'Freezing', 'Evaporation', 1);
